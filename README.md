65906cf327ab981ddea0fb7c1c2ee1cad5b7d6c8
Francisco <franciscogd@gatech.edu>
2018-05-02 15:27:32 -0400

    echo never > /sys/kernel/mm/transparent_hugepage/defrag with sudo su -c

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Cloudera Manager requires System configurations and a reboot before the installer can run.
 To use these scripts, you need to perform the following steps:
 
- (1) sudo yum install git
+ (1) sudo yum install -y git
  (2) git clone https://github.com/fgalindo7/cloudera-manager.git
  (3) bash cloudera-manager/cm_sys_config.sh
 	This will reboot your system
--- cm_sys_config.sh
+++ cm_sys_config.sh
@@ -7,12 +7,11 @@ cd /opt; sudo wget http://archive.cloudera.com/cm5/installer/latest/cloudera-man
 sudo chmod u+x /opt/cloudera-manager-installer.bin
 
 # Transparent Huge Page Compaction if enabled can cause significant performance problems
-sudo su - root
-echo never > /sys/kernel/mm/transparent_hugepage/defrag
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
+sudo su -c "echo never > /sys/kernel/mm/transparent_hugepage/defrag" root
+sudo su -c "echo never > /sys/kernel/mm/transparent_hugepage/enabled" root
 # Add these commands to an init script such as /etc/rc.local so it will be set on system reboot
 echo "if test -f /sys/kernel/mm/transparent_hugepage/enabled; then echo never > /sys/kernel/mm/transparent_hugepage/enabled; echo never > /sys/kernel/mm/transparent_hugepage/defrag; fi" >> /etc/rc.local
-exit
+
 
 # Cloudera recommends setting /proc/sys/vm/swappiness to a maximum of 10
 # Set the value for the running system
@@ -23,7 +22,7 @@ fi
 # Backup sysctl.conf
 sudo cp -p /etc/sysctl.conf /etc/sysctl.conf.`date +%Y%m%d-%H:%M`
 
-# Set the value in /etc/sysctl.conf so it stays after reboot.
+# Set the value in /etc/sysctl.conf to keep after reboot
 sudo sh -c 'echo "" >> /etc/sysctl.conf'
 sudo sh -c 'echo "# Devops" >> /etc/sysctl.conf'
 sudo sh -c 'echo "# Set swappiness to 10 as recommended by Cloudera Manager" >> /etc/sysctl.conf'
